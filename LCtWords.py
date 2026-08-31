067e30203c6a6f8f073d377150ec1eb766ed4b65
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-08-30 20:46:58 -0400

    update LCtWords.py

--- LCtWords.py
+++ LCtWords.py
@@ -1,27 +1,27 @@
 
 #330
-def get_lowercase_words(S)
+def get_lowercase_words(S):
 
-s_letters=(len(S))
-l_words = []
-words = S.split()
-word_num = len(words)
-lword_ct = 0 
+   s_letters=(len(S))
+   l_words = []
+   words = S.split()
+   word_num = len(words)
+   lword_ct = 0 
 
-print(words)
-print("# of chsracters ",s_letters)
-print("# of words: ",word_num)
+   print(words)
+   print("# of chsracters ",s_letters)
+   print("# of words: ",word_num)
 
 
-lword_ct = 0
-for ct in range(word_num):
-    if (words[ct].islower()==True):
-       l_words.append(words[ct])
-       lword_ct += 1
+   lword_ct = 0
+   for ct in range(word_num):
+      if (words[ct].islower()==True):
+         l_words.append(words[ct])
+         lword_ct += 1
       
-return(l_words)   
+   return(l_words)   
 
 instring=("hello GOOD world")
-answer=(get_lowercase_words(instring)
+answer=(get_lowercase_words(instring))
 
 print(answer)
