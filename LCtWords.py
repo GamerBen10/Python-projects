008138b753b7c1dbddf215a87823a30ed079aff8
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-08-31 02:40:46 -0400

    update LCtWords.py

--- LCtWords.py
+++ LCtWords.py
@@ -1,29 +1,14 @@
 
-#330
 def get_lowercase_words(S):
     l_words = []
     for word in S.split():
         if word.islower():
             l_words.append(word)
-          
     return " ".join(l_words)
 
 
 print(get_lowercase_words("hello GOOD world"))
-
-print(get_lowercase_words("these are all lower caae"))
-
-
-
-
-
-"""
-get_lowercase_words("less is NoT more")
-get_lowercase_words("DonT eat pizza every OTHER day")
-
-
-get_lowercase_words("the Super quick AND snEaky  rown fox Leapt anD jumped over aNd AROUND the lazy SloW dog")
-
-get_lowercase_words("hello GOOD world")
-"""
-
+print(get_lowercase_words("these are all lowercase"))
+print(get_lowercase_words("less is NoT more"))
+print(get_lowercase_words("DonT eat pizza every OTHER day"))
+print(get_lowercase_words("Super quick AND snEaky brown fox Leapt anD jumped over aNd AROUND the lazy SloW dog"))
