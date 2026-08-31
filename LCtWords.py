c16de754c95c5cbd385d38f79dab8f06c7f0f4f7
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-08-30 20:43:36 -0400

    update LCtWords.py

--- LCtWords.py
+++ LCtWords.py
@@ -1,12 +1,12 @@
-# lword_ct = 0 lower case words in sentence 
-#330
 
-print("Enter sentence to evaluwte:")
-S=input('')
+#330
+def get_lowercase_words(S)
+
 s_letters=(len(S))
 l_words = []
 words = S.split()
 word_num = len(words)
+lword_ct = 0 
 
 print(words)
 print("# of chsracters ",s_letters)
@@ -16,10 +16,12 @@ print("# of words: ",word_num)
 lword_ct = 0
 for ct in range(word_num):
     if (words[ct].islower()==True):
-       l_words += word(word_num)
+       l_words.append(words[ct])
        lword_ct += 1
       
-print(l_words)    
+return(l_words)   
 
+instring=("hello GOOD world")
+answer=(get_lowercase_words(instring)
 
-
+print(answer)
