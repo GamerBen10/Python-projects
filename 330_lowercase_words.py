747f78b7709928bede4a7ed949e34346a2a7f4e2
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-02 22:18:08 -0400

    update 330_lowercase_words.py

--- 330_lowercase_words.py
+++ 330_lowercase_words.py
@@ -7,5 +7,5 @@ word_count = len(words)
 
 for index in range (word_count):
    word_len = len(words[index]) 
-   print("word: ",words[index],"  length: ",word_len)  
+   print("word: ",words[index],"  length: ",word_len,"  ",word_len % 2)  
   
