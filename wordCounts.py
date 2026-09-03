2fdac61778131dd0db916fb7fe3921c7e53b6840
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-02 20:41:45 -0400

    update wordCounts.py

--- wordCounts.py
+++ wordCounts.py
@@ -1,4 +1,19 @@
 #Given a string of words, return only the words with an odd number of letters.
 
 def odd_words(sentence):
-l
+  pass
+
+sentence  =("this sentence has x number of odd letter words in it")
+oddz = 0
+
+print(sentence)
+letters = len(sentence)
+print("# of charactere: ",letters)
+words = (len(sentence.split()))
+print("# of words: ",words)
+
+for ct in range(words):
+    print (sentence[ct])
+  
+   
+
