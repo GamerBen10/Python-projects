7faf3896e5796ab6a8799b6e8c847d1e724d5713
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-03 00:59:57 -0400

    update 330_lowercase_words.py

--- 330_lowercase_words.py
+++ 330_lowercase_words.py
@@ -6,10 +6,13 @@ words = text.split()
 word_count = len(words)
 
 
-
+odd_ct = 0
 
 for index in range (word_count):
    word_len = len(words[index])
    odd = bool(word_len % 2)
+   if odd: odd_ct +=1
   
    print("word: ",words[index],"  length: ",word_len," Odd: ",odd)
+print("")
+print("number of odd wirds i",odd_ct)
