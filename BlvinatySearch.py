f09e50cf8553665b13322d14cfcf61cf2b9eac51
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-04 00:21:10 -0400

    update BlvinatySearch.py

--- BlvinatySearch.py
+++ BlvinatySearch.py
@@ -8,6 +8,7 @@ def bsearch(numz,num):
   
   length = len(numz)
   midz = length // 2 -1
+  print("midz ",midz)
   print("list: ",numz)
   print("find nun ",num)
 
@@ -15,12 +16,14 @@ def bsearch(numz,num):
     print("equal")
   elif int(numz[midz])>int(num):
     print(num, " is <",numz[midz])
+    midz /= 2
+    print("new midz",midz)
   else:
     print(num, " is > than ",numz[midz])
     
   
 numz = [4,8,15,16,34,
         37,42,46,50,55]  
-num = 50
+num = 8
 
 bsearch(numz, num)
