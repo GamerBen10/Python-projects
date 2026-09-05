1ece8106964840d710dcdb7ba222930b4c6298b2
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-04 21:12:48 -0400

    update cesarCypher.py

--- cesarCypher.py
+++ cesarCypher.py
@@ -3,9 +3,10 @@ def caesar(text, shift):
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet, shifted_alphabet)
     encrypted_text = text.translate(translation_table)
-    print(encrypted_text)
+
+    return encrypted_text
 
 encrypted_text = caesar('freeCodeCamp', 3)
 
-
+print(encrypted_text)
 
