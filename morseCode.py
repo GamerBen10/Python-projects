d804374b49464dc4897871393c64d6ae48e44037
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-05 02:39:42 -0400

    update morseCode.py

---
+++ morseCode.py
@@ -0,0 +1,62 @@
+def decode_morse(code):
+
+def char_to_morse(char: str) 
+
+    match char.upper():
+        case "A":
+            return ".-"
+        case "B":
+            return "-..."
+        case "C":
+            return "-.-."
+        case "D":
+            return "-.."
+        case "E":
+            return "."
+        case "F":
+            return "..-."
+        case "G":
+            return "--."
+        case "H":
+            return "...."
+        case "I":
+            return ".."
+        case "J":
+            return ".---"
+        case "K":
+            return "-.-"
+        case "L":
+            return ".-.."
+        case "M":
+            return "--"
+        case "N":
+            return "-."
+        case "O":
+            return "---"
+        case "P":
+            return ".--."
+        case "Q":
+            return "--.-"
+        case "R":
+            return ".-."
+        case "S":
+            return "..."
+        case "T":
+            return "-"
+        case "U":
+            return "..-"
+        case "V":
+            return "...-"
+        case "W":
+            return ".--"
+        case "X":
+            return "-..-"
+        case "Y":
+            return "-.--"
+        case "Z":
+            return "--.."
+        case " ":
+            return "/"  # Word separator
+        case _:
+            return "?"  # Unknown
+    return code
