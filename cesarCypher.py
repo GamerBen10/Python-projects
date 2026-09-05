87d92fa54bf42207b7ce09cb5a1eb588ec966ebd
mafmanet <59784416+GamerBen10@users.noreply.github.com>
2026-09-04 21:06:46 -0400

    update cesarCypher.py

--- cesarCypher.py
+++ cesarCypher.py
@@ -1,36 +1,11 @@
-alphalist = []
-shift = 5
+def caesar(text, shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
 
-
-# Populate the list using .append()
-for i in range(ord('A'), ord('Z') + 1):
-    alphalist.append(chr(i))
-print(alphalist)
-print(" ")
-
-# Cesar Cypher
-# Slice from index 5 to end, then append the first 5 characters
-shiftedalpha= alphalist[5:] + alphalist[:5]
-
-print(shiftedalpha)
-print(" ")
-
-lower_chars=("abc")
-upper_chars=("ABC")
-
-table_str = (lower_chars,upper_chars)
-print(table_str)
-
-
-"""
-mapping = {"a": "4", "e": "3", "o": "0"}
-table = str.maketrans(mapping)
-print("hello all".translate(table)) # Output: 'h3ll0 4ll'
-"""
-
-
-
-
+encrypted_text = caesar('freeCodeCamp', 3)
 
 
 
